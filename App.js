e69fc5f292963104216d9e8fc961365761897eaf
Armaan <atteshamshaikh47@gmail.com>
2025-06-08 21:59:12 +0530

    Epi 2 igniting app

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const haeding = React.createElement("div", { id: "container" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is an H1 tag"),
+    React.createElement("h1", {}, "This is an H1 tag of armaan"),
     React.createElement("h2", {}, "This is an H2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
